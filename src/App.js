a09a2d9d8c05a4d04ff80c31a6ac8c959b2fb179
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-03-18 20:30:57 +0400

    style cards

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header2 from "./Temp/Header/Header2";
 import List from "./Temp/List/List";
 function App() {
   return (
-    <>
+ 
+    <div className="container">
       <Header2 />
       <List />
       <Footer />
-    </>
+    </div>
+
+  
   );
 }
 
