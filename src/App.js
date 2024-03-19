c40f542e253850fb1aa44793d39240f62ca970bf
Ani <anibolokadze@gmail.com>
2024-03-20 01:14:51 +0400

    implemented markup for search feature and product catalog

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
-import Footer from "./Temp/Footer/Footer";
-import Header2 from "./Temp/Header/Header2";
-import List from "./Temp/List/List";
+import Header from "./Components/Header/Header";
+import Content from "./Components/Product/Product";
+import Footer from "./Components/Footer/Footer";
+import Search from "./Components/Search/Search";
+import data from "./data";
+
 function App() {
   return (
- 
-    <div className="container">
-      <Header2 />
-      <List />
-      <Footer />
+    <div className="App">
+      <Header {...data.header} />
+      <Search />
+      <main>
+        {data.content.map((content, index) => (
+          <Content
+            key={index}
+            title={content.title}
+            description={content.description}
+            imageUrl={content.imageUrl}
+            imageAlt={content.imageAlt}
+          />
+        ))}
+      </main>
+      <Footer {...data.footer} />
     </div>
-
-  
   );
 }
 
