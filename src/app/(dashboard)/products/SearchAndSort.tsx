6092adcc43b1b910a493e2c95725c30634cb073e
ani <anibolokadze@gmail.com>
2024-05-04 17:11:04 +0400

    added simple header styles

--- src/app/(dashboard)/products/SearchAndSort.tsx
+++ src/app/(dashboard)/products/SearchAndSort.tsx
@@ -49,7 +49,7 @@ const ProductList: React.FC<ProductProps> = ({ products }) => {
 
   return (
     <>
-      <form className="flex justify-center">
+      {/* <form className="flex justify-center">
         <input
           className="px-3 py-2"
           type="search"
@@ -60,6 +60,37 @@ const ProductList: React.FC<ProductProps> = ({ products }) => {
           value={inputValue}
           onChange={handleSearchInputChange}
         />
+      </form> */}
+
+      <form className="max-w-md mx-auto">
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder={t("search")}
+            autoComplete="off"
+            value={inputValue}
+            onChange={handleSearchInputChange}
+          />
+        </div>
       </form>
 
       <button type="button" onClick={sortProducts} className="mx-10 my-10">
