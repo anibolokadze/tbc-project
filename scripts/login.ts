98165619c08f83a380226655aea19e8cb8ef440e
ani <anibolokadze@gmail.com>
2024-05-10 15:19:33 +0400

    update logout function to use conditional API URL

--- scripts/login.ts
+++ scripts/login.ts
@@ -2,7 +2,14 @@ export const handleLogin = async (
   username: string,
   password: string
 ): Promise<void> => {
-  await fetch("http://localhost:3000/api/login", {
+  let apiUrl;
+  if (process.env.NODE_ENV === "production") {
+    apiUrl = "https://tbc-project-two.vercel.app/api/login";
+  } else {
+    apiUrl = "http://localhost:3000/api/login";
+  }
+
+  await fetch(apiUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
--- scripts/logout.ts
+++ scripts/logout.ts
@@ -1,5 +1,12 @@
 export const handleLogout = async () => {
-  await fetch("http://localhost:3000/api/logout", {
+  let apiUrl;
+  if (process.env.NODE_ENV === "production") {
+    apiUrl = "https://tbc-project-two.vercel.app/api/logout";
+  } else {
+    apiUrl = "http://localhost:3000/api/logout";
+  }
+
+  await fetch(apiUrl, {
     method: "POST",
   });
 };
