6d935d87c8f07d3c718d7108b7e7608ba1ef099c
ani <anibolokadze@gmail.com>
2024-05-12 22:56:42 +0400

    CORS headers

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,6 +3,18 @@ import { cookies } from "next/headers";
 export async function POST(request: any) {
   const { username, password } = await request.json();
 
+  const headers: HeadersInit = {
+    "Access-Control-Allow-Origin": "https://your-vercel-app.vercel.app",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Credentials": "true",
+    "Content-Type": "application/json",
+  };
+
+  if (request.method === "OPTIONS") {
+    return new Response(null, { headers });
+  }
+
   const response = await fetch("https://dummyjson.com/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
