818d7ff7cc735da098922274b38658735359ac3a
ani <anibolokadze@gmail.com>
2024-05-12 23:01:59 +0400

    error log

--- scripts/login.ts
+++ scripts/login.ts
@@ -4,12 +4,20 @@ export const handleLogin = async (
   username: string,
   password: string
 ): Promise<void> => {
-  await fetch(`${BASE_URL}/api/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
+  try {
+    const response = await fetch(`${BASE_URL}/api/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to login");
+    }
+  } catch (error: any) {
+    console.error("Error logging in:", error.message);
+  }
 };
