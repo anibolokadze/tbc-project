38f375763d3177ab4046dee0607ba625ad132cf7
ucha <uchakobakhidze@gmail.com>
2024-05-26 23:21:09 +0400

    first commit

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -7,7 +7,7 @@ const ProductItem: React.FC<cardProps> = ({
   handleClick,
 }) => {
   return (
-    <div className="flex flex-col gap-3 w-[250px] h-[370px] rounded-[10px] border border-blue-600 dark:border-light_blue overflow-hidden lg:w-[300px] lg:h-[470px] ">
+    <div className="flex flex-col gap-3 w-[250px] h-[380px] rounded-[10px] border border-blue-600 dark:border-light_blue overflow-hidden lg:w-[300px] lg:h-[490px] pb-3">
       <Link href={`/products/${id}`}>
         <div className="flex flex-col gap-3 w-full h-[310px] lg:h-[390px]">
           <div className="w-full min-h-[200px] max-h-[200px] lg:min-h-[250px] lg:max-h-[250px]">
@@ -23,21 +23,21 @@ const ProductItem: React.FC<cardProps> = ({
             Price: ${price}
           </p>
           <div className="flex flex-col gap-3 px-[18px] lg:px-[22px]">
-            <h5 className="text-[15px] leading-[24px] lg:text-[18px] lg:leading-[29px] text-center text-blue-600 dark:text-light_blue">
+            <h5 className="text-[15px] leading-[25px] lg:text-[18px] lg:leading-[29px] text-center text-blue-600 dark:text-light_blue">
               {title}
             </h5>
             <div className="flex flex-col gap-3 justify-between items-center">
               <div className="w-full h-[70px] lg:h-[90px] text-[11px] lg:text-[13px] text-wrap">
-                {description}
+                {description.slice(0,50)}...
               </div>
             </div>
           </div>
         </div>
       </Link>
-      <div className="w-full px-[35px] lg:px-[50px]">
+      <div className="w-full px-[35px] lg:px-[50px] mt-auto">
         <button
           onClick={() => handleClick(id)}
-          className="group w-full flex justify-center gap-3 items-center lg:py-2 border border-blue-600 dark:border-light_blue rounded-[10px] lg:rounded-[12px] "
+          className="group w-full flex justify-center gap-3 items-center lg:py-2 border border-blue-600 dark:border-light_blue rounded-[10px] lg:rounded-[12px] hover:bg-blue-600 transition-colors duration-300"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
