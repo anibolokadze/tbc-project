2ed004cbfd2a694ed98755813068d8051515c285
ani <anibolokadze@gmail.com>
2024-05-27 16:28:19 +0400

    remove comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
+// import { AUTH_COOKIE_KEY } from "../constants";
 import { getSession } from "@auth0/nextjs-auth0/edge";
-
 const protectedRoutes = [
   "/",
   "/about",
@@ -19,35 +19,34 @@ const publicRoutes = ["/login"];
 export default async function middleware(request: NextRequest) {
   const res = NextResponse.next();
 
-  try {
-    // Change: Added try-catch block to handle and log errors.
-    const session = await getSession(request, res);
-    const userId = session?.user?.sub;
+  const session = await getSession(request, res);
+  const userId = session?.user?.sub;
 
-    const path = request.nextUrl.pathname;
-    const isProtectedRoute =
-      protectedRoutes.includes(path) ||
-      path.includes("/posts") ||
-      path.includes("/products");
+  // const cookie = request.cookies.get(AUTH_COOKIE_KEY)?.value;
+  // let token = null;
 
-    const isPublicRoute = publicRoutes.includes(path);
+  // if (cookie) {
+  //   const cookieObject = JSON.parse(cookie);
+  //   if (cookieObject) {
+  //     token = cookieObject?.token;
+  //   }
+  // }
 
-    if (isProtectedRoute && !userId) {
-      console.log(`Redirecting to login from protected route: ${path}`); // Change: Added logging for debugging.
-      return NextResponse.redirect(new URL("/api/auth/login", request.nextUrl));
-    }
-    if (isPublicRoute && (userId === undefined || userId)) {
-      console.log(`Redirecting to home from public route: ${path}`); // Change: Added logging for debugging.
-      return NextResponse.redirect(new URL("/", request.nextUrl));
-    }
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute =
+    protectedRoutes.includes(path) ||
+    path.includes("/posts") ||
+    path.includes("/products");
 
-    console.log(`Allowing access to: ${path}`); // Change: Added logging for debugging.
-    return res;
-  } catch (error) {
-    // Change: Added error handling.
-    console.error("Middleware error:", error); // Change: Log the error.
-    return new NextResponse("Internal Server Error", { status: 500 }); // Change: Return 500 response in case of an error.
+  const isPublicRoute = publicRoutes.includes(path);
+
+  if (isProtectedRoute && !userId) {
+    return NextResponse.redirect(new URL("/api/auth/login", request.nextUrl));
   }
+  if (isPublicRoute && (userId === undefined || userId)) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+  return null;
 }
 
 export const config = {
